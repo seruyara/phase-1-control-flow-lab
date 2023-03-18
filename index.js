ad04727a1f2194d4b37c03c08b0383d3fe5d97f4
Sheila Seruya <Sheilaseruya@gmail.com>
2023-03-18 07:02:04 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400 ){
+    return 'This one is on me!'
+  }else if(2000<feet&&feet<2501){
+    return 'I will gladly take your thirty bucks.';
+  }else 
+    return 'No can do.' ; 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return "Ok, sounds good.";
+  } 
+  else {
+   return "No go.";
+  }
+   
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+   if (tip === 'generous'){
+    return  "Thank you so much."           
+   }
+   else if(tip === 'not as generous')
+   {
+    return "Thank you.";
+   }
+   else if ('anything else') {
+    return  "Bye." ;
+   }
+    
+   
 }
